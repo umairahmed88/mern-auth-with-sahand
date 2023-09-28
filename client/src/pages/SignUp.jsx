5fc24b137cfba62b5e7681dbdf0799e9fa15f788
umairahmed88 <umairahmed_88@ymail.com>
2023-09-28 12:41:29 +0500

    completed sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
 	const [formData, setFormData] = useState({});
 	const [error, setError] = useState(false);
 	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -30,6 +31,7 @@ export default function SignUp() {
 				setError(false);
 				return;
 			}
+			navigate("/sign-in");
 		} catch (error) {
 			setLoading(false);
 			setError(true);
@@ -67,7 +69,7 @@ export default function SignUp() {
 					disabled={loading}
 					className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
 				>
-					{loading ? "Loading..." : "Sign In"}
+					{loading ? "Loading..." : "Sign Up"}
 				</button>
 			</form>
 			<div className='flex gap-2 mt-5'>
